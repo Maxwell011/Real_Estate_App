42b0dc5738803c057c242d9b4741d5cc2faef2d5
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-08-04 22:39:17 +0100

    Added banner

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 import styles from "@/styles/Home.module.css";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
+import Property from '../components/Property';
 
-const inter = Inter({ subsets: ["latin"] });
 
 const Banner = ({
   purpose,
@@ -39,7 +39,7 @@ const Banner = ({
   </Flex>
 );
 
-export default function Home(propertiesForSale, propertiesForRent) {
+export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
     <>
       <Head>
@@ -49,39 +49,38 @@ export default function Home(propertiesForSale, propertiesForRent) {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className={`${styles.main} ${inter.className}`}>
-        <h1>Hello World</h1>
-
-        {/* Banner 1 */}
+      <Box>
         <Banner
           purpose='RENT A HOME'
-          title1='Rental Homes For'
+          title1='Rental Homes for'
           title2='Everyone'
-          desc1='Explore Apartments, Villas, Homes'
+          desc1=' Explore from Apartments, builder floors, villas'
           desc2='and more'
           buttonText='Explore Renting'
           linkName='/search?purpose=for-rent'
           imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
         />
         <Flex flexWrap='wrap'>
-          {/* Fetch the properties and map over them... */}
-          {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+          {propertiesForRent.map((property) => (
+            <Property property={property} key={property.id} />
+          ))}
         </Flex>
-
-        {/* Banner 2 */}
         <Banner
           purpose='BUY A HOME'
-          title1='Find, Buy & Own Your'
+          title1=' Find, Buy & Own Your'
           title2='Dream Home'
-          desc1='Explore Apartments, Villas, Homes'
-          desc2='and more'
+          desc1=' Explore from Apartments, land, builder floors,'
+          desc2=' villas and more'
           buttonText='Explore Buying'
           linkName='/search?purpose=for-sale'
           imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
         />
-                  {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
-
-      </main>
+        <Flex flexWrap='wrap'>
+          {propertiesForSale.map((property) => (
+            <Property property={property} key={property.id} />
+          ))}
+        </Flex>
+      </Box>
     </>
   );
 }
